e646ed9f5adf59ab8f1203a8c6039d9817c7ec14
brendan <lobrendan@gmail.com>
2022-01-18 17:01:46 -0800

    working on fixing submit button

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 "use strict";
 
-$('#search-btn').on('click', makeSearch);
+$('#giphy-form').on('submit', makeSearch);
 
-async function makeSearch() {
+/** makeSearch grabs the #search-term value and makes a call to the Giphy API,
+ * then passes the first gif URL to appendGif
+ */
+async function makeSearch(evt) {
+    evt.preventDefault();
+
     let searchTerm = $('#search-term').val();
-    let params = { q: searchTerm };
-    console.log("params = ", params);
+    let searchParams = { q: searchTerm, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" };
+    console.log("params = ", { searchParams });
     let response = await axios.get(
-        'http://api.giphy.com/v1/gifs/search?', params);
-        console.log("response: ", response.data);
+        'http://api.giphy.com/v1/gifs/search?', { params: searchParams });
+    console.log("response: ", response.data);
 
     appendGif(response.data[0].embed_url);
 }
 
-function appendGif (gifUrl) {
+/** appendGif takes a string URL argument, then creates an img
+ *  element, sets the source URL to the gifURL, and appends it to
+ * the #gifs-container div
+ */
+
+function appendGif(gifUrl) {
     let $newGifImg = $(`<img class = 'gif' src = ${gifUrl}>`);
     $("#gifs-container").append($newGifImg);
 }
