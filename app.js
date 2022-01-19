10ccf8d84e855852fbecd35f27b9ef1630a34cfc
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-01-18 16:34:00 -0800

    adds makeSearch async function and appendGif function

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ $('#search-btn').on('click', makeSearch);
 async function makeSearch() {
     let searchTerm = $('#search-term').val();
     let params = { q: searchTerm };
+    console.log("params = ", params);
     let response = await axios.get(
         'http://api.giphy.com/v1/gifs/search?', params);
+        console.log("response: ", response.data);
 
-    console.log("response: ", response);
+    appendGif(response.data[0].embed_url);
+}
 
-    appendGif(response.data);
+function appendGif (gifUrl) {
+    let $newGifImg = $(`<img class = 'gif' src = ${gifUrl}>`);
+    $("#gifs-container").append($newGifImg);
 }
