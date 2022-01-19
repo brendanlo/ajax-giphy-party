af66cca0351b32b0a9e039442877cf2d5146bade
brendan <lobrendan@gmail.com>
2022-01-18 16:04:31 -0800

    started html & js files

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Let's get this party started!");
+"use strict";
+
+$('#search-btn').on('click', makeSearch);
+
+async function makeSearch() {
+    let searchTerm = $('#search-term').val();
+    let params = { q: searchTerm };
+    let response = await axios.get(
+        'http://api.giphy.com/v1/gifs/search?', params);
+
+    console.log("response: ", response);
+
+    appendGif(response.data);
+}
